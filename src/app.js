3c9ea8875f42ba56015dc58b2ef4279a54330e9c
SkyMountain <skvn.clew@gmail.com>
2018-05-26 00:45:25 +0700

    add func mustBeUser in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user.model');
 // const parser = require('body-parser').urlencoded({extended: false});
 const parser = require('body-parser').json();
+const { sign, verify} = requie('./lib/jwt.js');
 
 app.post('/signup',parser, (req,res) => {
     const { email, password, name, phone} = req.body;
@@ -17,4 +18,24 @@ app.post('/signin',parser, (req,res) => {
     .then(user => res.send({ success: true, user}) )
     .catch(error => res.status(error.statusCode).send({ success: false, message: error.message, code: error.code}) );
 });
+
+function mustBeUser(req,res,next) {
+    const {token} = req.headers;
+    if (!token) return res.status(400).send({ success: false, message: " Invalid token"});
+    verify(token)
+    .then(obj => {
+        req.idUser = obj._id;
+        next();
+    })
+    .catch( () => res.status(400).send({ success: false, message: " Invalid token"}) );
+}
+
+app.post('/story', mustBeUser, (req,res) => {
+
+});
+
+app.delete('/story/id',  mustBeUser, (req,res) => {
+
+});
+
 module.exports = app;
